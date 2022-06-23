7997926bc423e62a5da57cf6b97627c567c4868a
Daniel Semblano <dsemblano@gmail.com>
2022-06-23 15:00:08 -0300

    Noticias styling homepage

--- site/web/app/themes/pros90amapatema/resources/views/partials/allposts.blade.php
+++ site/web/app/themes/pros90amapatema/resources/views/partials/allposts.blade.php
@@ -1,40 +1,46 @@
-<?php $wpb_all_query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
-
-<?php if ( $wpb_all_query->have_posts() ) : ?>
-
-<!-- the loop -->
-<?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
-<article @php(post_class('article-posts w-full xl:w-1/3 md:w-1/2 mt-4 pr-0 xl:pr-4'))>
-    <div class="box bg-white p-6 rounded-lg">
-        <figure class="thumbnail">
-            <a href="{{ get_permalink() }}">
-            @if ( has_post_thumbnail() )
-            {{ the_post_thumbnail('post-thumbnail', ['class' => 'lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-6', 'title' => 'Feature image']); }}
-            @else
-            <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-6" src="@asset('images/default.png')"  alt="<?php the_title(); ?>">
-            @endif
-            </a>
-        </figure>
-            <header>
-            <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
-                <a href="{{ get_permalink() }}"><?php the_title(); ?></a>
-            </h2>
-        
-            @include('partials.entry-meta')
-            </header>
-            <?php
-                //echo '<p class="leading-relaxed text-base text-gray-600 mt-2">' . get_the_excerpt() . '</p>'; 
-            ?>
-        </div>
-</article>
-<?php endwhile; ?>
-<!-- end of the loop -->
-
-
-<?php wp_reset_postdata(); ?>
-
-<?php else : ?>
-<p>
-    <?php _e( 'Sorry, no posts matched your criteria.' ); ?>
-</p>
-<?php endif; ?>
+<div class="noticias w-full">
+    <div class="page-header text-center container mt-4 mb-0 lg:mb-10 prose lg:prose-xl prose-img:rounded-xl prose-a:text-blue-600 hover:prose-a:text-blue-500">
+        <h2>Notícias</h2>
+    </div>
+    <div class="flex flex-wrap -m-4">
+        <?php $wpb_all_query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
+        <?php if ( $wpb_all_query->have_posts() ) : ?>
+        <!-- the loop -->
+        <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
+        <article @php(post_class('article-posts w-full xl:w-1/3 md:w-1/2 mt-4 pr-0 xl:pr-4'))>
+            <div class="box bg-white p-6 rounded-lg">
+                <figure class="thumbnail">
+                    <a href="{{ get_permalink() }}">
+                        @if ( has_post_thumbnail() )
+                        {{ the_post_thumbnail('post-thumbnail', ['class' => 'lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72
+                        rounded w-full object-cover object-center mb-6', 'title' => 'Feature image']); }}
+                        @else
+                        <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-6"
+                            src="@asset('images/default.png')" alt="<?php the_title(); ?>">
+                        @endif
+                    </a>
+                </figure>
+                <header>
+                    <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                        <a href="{{ get_permalink() }}">
+                            <?php the_title(); ?>
+                        </a>
+                    </h2>
+    
+                    @include('partials.entry-meta')
+                </header>
+                <?php
+                    //echo '<p class="leading-relaxed text-base text-gray-600 mt-2">' . get_the_excerpt() . '</p>'; 
+                ?>
+            </div>
+        </article>
+        <?php endwhile; ?>
+        <!-- end of the loop -->
+        <?php wp_reset_postdata(); ?>
+        <?php else : ?>
+        <p>
+            <?php _e( 'Sem posts.' ); ?>
+        </p>
+        <?php endif; ?>
+    </div>    
+</div>
--- site/web/app/themes/pros90amapatema/resources/views/template-home.blade.php
+++ site/web/app/themes/pros90amapatema/resources/views/template-home.blade.php
@@ -5,10 +5,6 @@
 @extends('layouts.app')
 
 @section('content')
-<?php $the_query = new WP_Query( array( 'posts_per_page' => 5, 'offset' => 3 ) ); ?>
-<div class="flex flex-wrap -m-4">
   {!! do_shortcode('[wdi_feed id="1"]') !!}
   @include('partials.allposts')
-</div>
-    {{-- {!! do_shortcode('[contact-form-7 id="20" title="Formulario"]') !!} --}}
 @endsection
