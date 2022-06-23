d5ea3aee454ac070889306e9c261b46b078e6a31
Daniel Semblano <dsemblano@gmail.com>
2022-06-23 15:45:47 -0300

    article fix

--- site/web/app/themes/pros90amapatema/resources/views/partials/content.blade.php
+++ site/web/app/themes/pros90amapatema/resources/views/partials/content.blade.php
@@ -1,4 +1,4 @@
-<article @php(post_class('article-posts xl:w-1/3 md:w-1/2 mt-4'))>
+<article @php(post_class('article-posts w-full xl:w-1/3 md:w-1/2 mt-4 pr-0 xl:pr-4'))>
   <div class="box bg-white p-6 rounded-lg">
     <figure class="thumbnail">
       <a href="{{ get_permalink() }}">
