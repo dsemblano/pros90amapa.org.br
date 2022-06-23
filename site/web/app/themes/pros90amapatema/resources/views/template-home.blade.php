73449de20c709678213b16752b2690f4d23a4263
Daniel Semblano <dsemblano@gmail.com>
2022-06-23 14:30:36 -0300

    Instagram shortcode

--- site/web/app/themes/pros90amapatema/resources/views/template-home.blade.php
+++ site/web/app/themes/pros90amapatema/resources/views/template-home.blade.php
@@ -7,6 +7,7 @@
 @section('content')
 <?php $the_query = new WP_Query( array( 'posts_per_page' => 5, 'offset' => 3 ) ); ?>
 <div class="flex flex-wrap -m-4">
+  {!! do_shortcode('[wdi_feed id="1"]') !!}
   @include('partials.allposts')
 </div>
     {{-- {!! do_shortcode('[contact-form-7 id="20" title="Formulario"]') !!} --}}
