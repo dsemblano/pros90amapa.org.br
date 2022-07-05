4e5889283e8f0aaa807986c33c5a1c4c69548ce8
Daniel Semblano <dsemblano@gmail.com>
2022-07-05 16:43:48 -0300

    Candidatos page new query

---
+++ site/web/app/themes/pros90amapatema/resources/views/partials/childpages.blade.php
@@ -0,0 +1,40 @@
+<div class="noticias w-full">
+    <div class="page-header text-center container mb-0 lg:mt-12 lg:mb-8 prose lg:prose-xl  prose-a:text-blue-600 hover:prose-a:text-blue-500">
+        <h2>Pré-Canditados 2022</h2>
+    </div>
+    <div class="flex flex-wrap">
+    <?php
+        $my_wp_query = new WP_Query();
+        $all_wp_pages = $my_wp_query->query(array('post_type' => 'page', 'posts_per_page' => -1));
+        // Get the page as an Object
+        $portfolio =  get_page_by_path('pre-candidatos-2022');
+        // Filter through all pages and find Portfolio's children
+        $portfolio_children = get_page_children( $portfolio->ID, $all_wp_pages );
+    ?>
+    @foreach ($portfolio_children as $children)
+    <article id="<?= $children->post_name ?>" @php(post_class('article-posts w-full xl:w-1/3 md:w-1/2 mt-4 pr-0 xl:pr-8 xl:pb-8'))>
+        <div class=" box bg-white">
+            <figure class="thumbnail">
+                <a href="<?= $children->guid ?>">
+                    <?= get_the_post_thumbnail( $children->ID) ?>
+                </a>
+            </figure>
+            <header class="p-6">
+                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                    <a href="{{ get_permalink() }}">
+                        <?php the_title(); ?>
+                    </a>
+                </h2>
+
+                @include('partials.entry-meta')
+            </header>
+            <?php
+                //echo '<p class="leading-relaxed text-base text-gray-600 mt-2">' . get_the_excerpt() . '</p>'; 
+            ?>
+        </div>
+    </article>
+    @endforeach
+    <!-- end of the loop -->
+    <?php wp_reset_postdata(); ?>
+@endsection
+</div>
