059d3f51ca19520fab897062f5aa77e8f11650d1
Daniel Semblano <dsemblano@gmail.com>
2022-07-05 17:58:37 -0300

    home pros amapa

--- site/web/app/themes/pros90amapatema/resources/views/partials/home/section1.blade.php
+++ site/web/app/themes/pros90amapatema/resources/views/partials/home/section1.blade.php
@@ -1,8 +1,8 @@
 <section class="section1 flex flex-wrap w-full flex-col md:flex-row items-center border-bs container">
     <div class="flex flex-col w-full justify-center items-start text-center md:text-left">
-      <h1 class="my-4 text-5xl font-bold leading-tight">PROS 90 Amapá</h1>
+      <h1 class="my-4 text-5xl font-bold leading-tight">PROS Amapá</h1>
       <p class="leading-normal text-xl mb-8">
-        PROS 90 Amapá - Partido Republicano da Ordem Social
+        PROS Amapá - Partido Republicano da Ordem Social
       </p>
       <div class="social lg:block w-full">
         <div class="mb-6 flex flex-col fill-pros90blue">
