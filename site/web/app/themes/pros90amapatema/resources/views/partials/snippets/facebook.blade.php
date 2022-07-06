4d57f1d7b57e2305346c877889cab2af95345c6a
Daniel Semblano <dsemblano@gmail.com>
2022-07-06 15:21:56 -0300

    facebook timeline false

--- site/web/app/themes/pros90amapatema/resources/views/partials/snippets/facebook.blade.php
+++ site/web/app/themes/pros90amapatema/resources/views/partials/snippets/facebook.blade.php
@@ -1,5 +1,5 @@
 <section class="social block lg:inline text-center w-full fill-pros90blue">
-    <div class="fb-page" data-href="https://www.facebook.com/prosamapa90" data-tabs="timeline" data-width="500"
+    <div class="fb-page" data-href="https://www.facebook.com/prosamapa90" data-tabs="" data-width="500"
         data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false"
         data-show-facepile="false">
         <blockquote cite="https://www.facebook.com/prosamapa90" class="fb-xfbml-parse-ignore"><a
@@ -8,7 +8,7 @@
 </section>
 
 <section class="social block lg:inline text-center w-full fill-pros90blue">
-    <div class="fb-page" data-href="https://www.facebook.com/prosmulheramapa" data-tabs="timeline" data-width="500"
+    <div class="fb-page" data-href="https://www.facebook.com/prosmulheramapa" data-tabs="" data-width="500"
         data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false"
         data-show-facepile="false">
         <blockquote cite="https://www.facebook.com/prosmulheramapa" class="fb-xfbml-parse-ignore"><a
