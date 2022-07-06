de51904d285bf00bebf3b249e0dc7396f1afff34
Daniel Semblano <dsemblano@gmail.com>
2022-07-06 16:05:37 -0300

    Typography fix

--- site/web/app/themes/pros90amapatema/resources/views/partials/allposts.blade.php
+++ site/web/app/themes/pros90amapatema/resources/views/partials/allposts.blade.php
@@ -1,6 +1,6 @@
 <div class="noticias w-full">
-    <div class="page-header text-center container my-8 lg:mt-12 lg:mb-8 prose lg:prose-xl  prose-a:text-blue-600 hover:prose-a:text-blue-500">
-        <h2>Notícias</h2>
+    <div class="page-header text-center container my-8 lg:mt-12 lg:mb-8 prose lg:prose-2xl  prose-a:text-blue-600 hover:prose-a:text-blue-500">
+        <h2>Últimas Notícias</h2>
     </div>
     <div class="flex flex-wrap">
         <?php $wpb_all_query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
