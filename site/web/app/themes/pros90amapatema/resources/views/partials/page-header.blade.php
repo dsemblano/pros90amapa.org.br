4f1f7518a510aae83f15fb8d6aaccc9a1f086b59
Daniel Semblano <dsemblano@gmail.com>
2022-07-07 18:01:01 -0300

    Animate effect to all h1

--- site/web/app/themes/pros90amapatema/resources/views/partials/page-header.blade.php
+++ site/web/app/themes/pros90amapatema/resources/views/partials/page-header.blade.php
@@ -1,5 +1,5 @@
 <?php echo do_shortcode( '[flexy_breadcrumb]'); ?> 
 
 <div class="page-header text-center container mt-4 my-8 lg:mb-10 prose lg:prose-2xl prose-a:text-blue-600 hover:prose-a:text-blue-500">
-  <h1 class="text-pros90lightblue">{!! $title !!}</h1>
+  <h1 class="text-pros90lightblue animated animatedFadeInUp fadeInUp">{!! $title !!}</h1>
 </div>
