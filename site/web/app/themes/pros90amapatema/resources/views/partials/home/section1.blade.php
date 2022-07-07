72e0ac41f69e9934cbc0e9be94b23850d7ee4b7f
Daniel Semblano <dsemblano@gmail.com>
2022-07-07 18:04:40 -0300

    line height h1 home

--- site/web/app/themes/pros90amapatema/resources/views/partials/home/section1.blade.php
+++ site/web/app/themes/pros90amapatema/resources/views/partials/home/section1.blade.php
@@ -1,7 +1,7 @@
 <section class="section1 flex flex-wrap w-full flex-col md:flex-row items-center border-bs container">
     <div class="flex flex-col w-full justify-center items-start text-center md:text-left">
       <h1 class="my-4 text-8xl font-bold leading-tight text-pros90lightblue animated animatedFadeInUp fadeInUp">PROS Amapá</h1>
-      <p class="leading-normal text-xl mb-8 animated animatedFadeInUp fadeInUp">
+      <p class="leading-none text-xl mb-8 animated animatedFadeInUp fadeInUp">
         PROS Amapá - Partido Republicano da Ordem Social
       </p>
       <div class="social lg:block w-full">
