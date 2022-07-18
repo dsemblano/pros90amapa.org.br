89a7c972676a9c2851e2331c9a5fc96b0e363f4a
Daniel Semblano <dsemblano@gmail.com>
2022-07-18 12:15:47 -0300

    styling home margin, padding, typography

--- site/web/app/themes/pros90amapatema/resources/views/partials/home/section1.blade.php
+++ site/web/app/themes/pros90amapatema/resources/views/partials/home/section1.blade.php
@@ -1,17 +1,19 @@
 <section class="section1 flex flex-wrap w-full flex-col md:flex-row items-center border-bs container">
     <div class="flex flex-col w-full justify-center items-start text-center md:text-left">
-      <h1 class="my-4 text-8xl font-bold leading-tight text-pros90lightblue animated animatedFadeInUp fadeInUp">PROS Amapá</h1>
-      <p class="leading-none text-xl mb-8 animated animatedFadeInUp fadeInUp">
-        PROS Amapá - Partido Republicano da Ordem Social - PROS AP. 
-      </p>
-      <p class="leading-none text-xl mb-8 animated animatedFadeInUp fadeInUp">
-        Sede do partido: Av. Henrique Galúcio 1081, Bairro Central, Macapá AP.
-      </p>
-      <p class="leading-none text-xl mb-8 animated animatedFadeInUp fadeInUp">
-        Toda grande jornada sempre começa com passos firmes!
-      </p>
+      <h1 class="my-4 text-8xl font-bold leading-none text-pros90lightblue animated animatedFadeInUp fadeInUp">PROS Amapá</h1>
+      <article class="mb-4 lg:mb-9 prose lg:prose-2xl prose-a:text-blue-800 prose-a:no-underline hover:prose-a:text-blue-500 hover:prose-a:underline animated animatedFadeInUp fadeInUp">
+        <h2>
+          Partido Republicano da Ordem Social<br>PROS AP
+        </h2>
+        <p>
+          Sede do partido: Av. Henrique Galúcio 1081, Bairro Central, Macapá AP.
+        </p>
+        <p>
+          Toda grande jornada sempre começa com passos firmes!
+        </p>
+      </article>
       <div class="social lg:block w-full">
-        <div class="mb-6 flex flex-col fill-pros90blue">
+        <div class="mb-4 lg:mb-9 flex flex-col fill-pros90blue">
           @include('partials/snippets.socialicons')
           @include('partials/snippets.fblikeshare')
         </div>
