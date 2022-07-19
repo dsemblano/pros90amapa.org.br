c6e22a0c42d0ef24c0fd69dff9225a39e8fc4c8b
Daniel Semblano <dsemblano@gmail.com>
2022-07-19 15:35:20 -0300

    home link to pros nacional

--- site/web/app/themes/pros90amapatema/resources/views/partials/home/section1.blade.php
+++ site/web/app/themes/pros90amapatema/resources/views/partials/home/section1.blade.php
@@ -6,7 +6,7 @@
           Partido Republicano da Ordem Social<br>PROS AP
         </h2>
         <p>
-          Sede do partido: Av. Henrique Galúcio 1081, Bairro Central, Macapá AP.
+          Diretório Estadual do <a href="https://pros.org.br" data-id="https://pros.org.br" target="_blank" rel="noopener" data-type="URL">PROS Nacional</a>.<br>Sede do partido: Av. Henrique Galúcio 1081, Bairro Central, Macapá AP.
         </p>
         <p>
           Toda grande jornada sempre começa com passos firmes!
