b1ea4ce7f6067831d9b9b855fd905db5d3865cda
Daniel Semblano <dsemblano@gmail.com>
2022-07-22 19:58:59 -0300

    New instagram plugin Smash Ballon

--- site/web/app/themes/pros90amapatema/resources/views/template-home.blade.php
+++ site/web/app/themes/pros90amapatema/resources/views/template-home.blade.php
@@ -6,6 +6,7 @@
 
 @section('content')
 @include('partials/home.section1')
-  {!! do_shortcode('[wdi_feed id="1"]') !!}
+  {{-- {!! do_shortcode('[wdi_feed id="1"]') !!} --}}
+  {!! do_shortcode('[instagram-feed feed=1]') !!}
   {{-- @include('partials.allposts') --}}
 @endsection
