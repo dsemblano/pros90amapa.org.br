57047d67e45033bb67915a914af0b40fbb5a2264
Daniel Semblano <dsemblano@gmail.com>
2022-07-27 17:34:21 -0300

    Candidatos page container fix updated

--- site/web/app/themes/pros90amapatema/resources/views/partials/childpages.blade.php
+++ site/web/app/themes/pros90amapatema/resources/views/partials/childpages.blade.php
@@ -3,7 +3,10 @@
     <div class="page-header text-center container my-8 lg:mt-12 lg:mb-8 prose lg:prose-2xl  prose-a:text-blue-800 prose-a:no-underline hover:prose-a:text-blue-500 hover:prose-a:underline">
         <h1 class="text-pros90lightblue animated animatedFadeInUp fadeInUp">Candidatos ao PROS AP em 2022</h1>
     </div>
-    <p class="lg:mb-8 prose lg:prose-2xl whitespace-nowrap">Conheça nossos candidatos que irão concorrer a uma vaga para deputado estadual na Assembléia Legislativa:</p>
+    <article class="container lg:mb-8 prose lg:prose-2xl ">
+        <p>Conheça nossos candidatos que irão concorrer a uma vaga para deputado estadual na Assembléia Legislativa:</p>
+        <p>Eles são: Carlos Lobato Cláudio Vigilante Diego Veículos Edinoelson Careca Inês Vale Ramos Lucas da Gazeta Maria Cristina Melque Gama Radialista Careca Almeida Rayfran Beirão Regiane Costa (Maju) Rosa Amaral e Simon Dias</p>
+    </article>
     <div class="flex flex-wrap">
     <?php
         $my_wp_query = new WP_Query();
