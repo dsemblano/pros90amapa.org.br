d207157d5ef330745d31d88589a3360e98ee295c
Daniel Semblano <dsemblano@gmail.com>
2022-08-08 12:13:58 -0300

    new home new logo

--- site/web/app/themes/pros90amapatema/resources/views/partials/home/section1.blade.php
+++ site/web/app/themes/pros90amapatema/resources/views/partials/home/section1.blade.php
@@ -1,23 +1,28 @@
-<section class="section1 flex flex-wrap w-full flex-col md:flex-row items-center border-bs container lg:mb-12">
-    <div class="flex flex-col w-full justify-center items-start text-center md:text-left">
-      <h1 class="mb-4 text-8xl font-bold leading-none text-pros90lightblue animated animatedFadeInUp fadeInUp">PROS Amapá</h1>
-      <article class="mb-4 lg:mb-9 prose lg:prose-2xl prose-a:text-blue-800 prose-a:no-underline hover:prose-a:text-blue-500 hover:prose-a:underline animated animatedFadeInUp fadeInUp">
+<section class="flex flex-col lg:flex-row lg:mt-40 justify-between items-start bg-white">
+    <div class="flex flex-col w-full lg:w-2/5 justify-center text-center lg:text-left animated animatedFadeInUp fadeInUp">
+      <h1 class="mb-4 uppercase text-7xl lg:text-9xl font-bold text-pros90lightblue">PROS Amapá</h1>
+      <img class="block lg:hidden rounded-tl-extraLarge rounded-br-extraLarge" width="600" height="718" alt="Logo PROS na home" src="@asset('images/logopros.png')" />
+      <article class="mt-8 mb-8 prose lg:prose-2xl prose-a:text-blue-800 prose-a:no-underline hover:prose-a:text-blue-500 hover:prose-a:underline">
         <h2>
           Partido Republicano da Ordem Social<br>PROS AP
         </h2>
-        <p>
+        <p class="leading-normal text-xl mb-8">
           Diretório Estadual do <a href="https://pros.org.br" data-id="https://pros.org.br" target="_blank" rel="noopener" data-type="URL">PROS Nacional</a>.<br>Sede do partido: Av. Henrique Galúcio 1081, Bairro Central, Macapá AP.
         </p>
-        <p>
+        <p class="leading-normal text-xl mb-8">
           Toda grande jornada sempre começa com passos firmes!
         </p>
-      </article>
-      <div class="social lg:block w-full">
-        <div class="mb-4 lg:mb-9 flex flex-col fill-pros90blue">
-          @include('partials/snippets.socialicons')
-          @include('partials/snippets.fblikeshare')
+        <div class="social lg:block w-full">
+          <div class="mb-4 lg:mb-9 flex flex-col fill-pros90blue">
+            @include('partials/snippets.socialicons')
+            @include('partials/snippets.fblikeshare')
+          </div>
+            @include('partials/snippets.facebook')
         </div>
-          @include('partials/snippets.facebook')
-      </div>
+      </article>
+    </div>
+    <!--Right Col desktop -->
+    <div class="social hidden lg:block w-full pl-48">
+      <img class="z-50 hidden lg:block rounded-tl-extraLarge rounded-br-extraLarge" width="600" height="718" alt="Logo PROS na home" src="@asset('images/logopros.png')" />
     </div>
   </section>
