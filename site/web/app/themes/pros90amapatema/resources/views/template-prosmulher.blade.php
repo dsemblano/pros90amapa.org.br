2ed7e1acde15cf70ac28fec96ddee2f459d73aee
Daniel Semblano <dsemblano@gmail.com>
2022-08-09 09:47:52 -0300

    pros mulher image h1 name

--- site/web/app/themes/pros90amapatema/resources/views/template-prosmulher.blade.php
+++ site/web/app/themes/pros90amapatema/resources/views/template-prosmulher.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 <div class="prosmulher w-full">
     <div class="page-header text-center container prose lg:prose-2xl  prose-a:text-blue-800 prose-a:no-underline hover:prose-a:text-blue-500 hover:prose-a:underline">
-        <h1>PROS Mulher</h1>
+        <h1>PROS Mulher Amapá</h1>
     </div>
     <figure class="grid my-8 place-items-center">
         <img src="@asset('images/logoprosmulhercomcandidatas.jpeg')" alt="Foto candidatas PROS">
