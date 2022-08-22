18c09d70837dd098ab5efc7126a2341da7603fe7
Daniel Semblano <dsemblano@gmail.com>
2022-08-22 10:50:26 -0300

    md resolution fix, animated.css

--- site/web/app/themes/pros90amapatema/resources/views/partials/home/section1.blade.php
+++ site/web/app/themes/pros90amapatema/resources/views/partials/home/section1.blade.php
@@ -1,7 +1,7 @@
 <section id="section1" class="container px-8 lg:px-16 flex flex-col lg:flex-row lg:mt-40 justify-between items-start bg-white relative">
-    <div class="flex flex-col w-full lg:w-5/6 justify-center text-center lg:text-left animated animatedFadeInUp fadeInUp">
+    <div class="flex flex-col w-full lg:w-5/6 justify-center text-center lg:text-left animate__animated animate__fadeInLeftBig">
       <h1 class="mb-4 uppercase text-7xl lg:text-9xl font-bold text-pros90lightblue">PROS Amapá</h1>
-      <img class="block lg:hidden rounded-tl-extraLarge rounded-br-extraLarge" width="600" height="718" alt="Logo PROS na home" src="@asset('images/logopros.png')" />
+      <img class="block lg:hidden" width="600" height="718" alt="Logo PROS na home" src="@asset('images/logopros.png')" />
       <article class="mt-8 mb-8 prose lg:prose-2xl prose-a:text-blue-800 prose-a:no-underline hover:prose-a:text-blue-500 hover:prose-a:underline">
         <h2>
           Partido Republicano da Ordem Social<br>PROS AP
@@ -23,6 +23,6 @@
     </div>
     <!--Right Col desktop -->
     <div class="social hidden lg:block w-full pl-48">
-      <img class="z-50 hidden lg:block rounded-tl-extraLarge rounded-br-extraLarge" width="600" height="718" alt="Logo PROS na home" src="@asset('images/logopros.png')" />
+      <img class="z-50 hidden lg:block" width="600" height="718" alt="Logo PROS na home" src="@asset('images/logopros.png')" />
     </div>
   </section>
