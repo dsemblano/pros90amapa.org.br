c99167b7b8ba3eaef26e1bebad7d3ac21819923a
Daniel Semblano <dsemblano@gmail.com>
2022-08-22 19:15:40 -0300

    the_excerpt on search page

--- site/web/app/themes/pros90amapatema/resources/views/partials/content-search.blade.php
+++ site/web/app/themes/pros90amapatema/resources/views/partials/content-search.blade.php
@@ -18,8 +18,8 @@
     
       @include('partials.entry-meta')
       </header>
-        <?php
-          //echo '<p class="leading-relaxed text-base text-gray-600 mt-2">' . get_the_excerpt() . '</p>'; 
-        ?>
+      <div class="entry-summary px-6 py-3">
+        @php(the_excerpt())
+      </div>
   </div>
 </article>
