bb02702ea0dcaec25e69396781f2517d5bd1cd04
Daniel Semblano <dsemblano@gmail.com>
2022-08-26 09:13:16 -0300

    template dias eleicoes css

--- site/web/app/themes/pros90amapatema/resources/views/template-dias.blade.php
+++ site/web/app/themes/pros90amapatema/resources/views/template-dias.blade.php
@@ -1,11 +1,11 @@
 {{--
   Template Name: Dias Template
 --}}
-<div id="countdown_container" class="container w-full h-auto flex flex-col items-center mt-12">
-    <a href="{{ home_url('/') }}">
-        <img width="300" height="418" alt="Logo PROS na home" src="@asset('images/logo.png')" />
-    </a>
+<div id="countdown_container" class="container w-full h-auto flex flex-col items-center mt-6">
     @include('partials/snippets.dias')
+    <a href="{{ home_url('/') }}">
+        <img alt="Logo PROS na home" src="@asset('images/smalllogo.png')" />
+    </a>
     <div class="text-4xl w-full font-bold relative">
         {!! do_shortcode('[countdown date="02 October 2022" width="600" color="#F45F2D" bgcolor="#FFF" format="dHMS" hour="07"]') !!}
     </div>
