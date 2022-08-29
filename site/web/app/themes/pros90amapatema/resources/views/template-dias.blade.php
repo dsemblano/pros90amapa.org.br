990cc21b79cb977036b45640231cb92d373e552a
Daniel Semblano <dsemblano@gmail.com>
2022-08-29 16:46:22 -0300

    logo jpg tmeplate dias fix

--- site/web/app/themes/pros90amapatema/resources/views/template-dias.blade.php
+++ site/web/app/themes/pros90amapatema/resources/views/template-dias.blade.php
@@ -3,9 +3,9 @@
 --}}
 <div id="countdown_container" class="container w-full h-auto flex flex-col items-center mt-6">
     @include('partials/snippets.dias')
-    <a href="{{ home_url('/') }}">
+    {{-- <a href="{{ home_url('/') }}">
         <img alt="Logo PROS na home" width="300" src="@asset('images/logodias.jpg')" />
-    </a>
+    </a> --}}
     <div class="text-4xl w-full font-bold relative mb-6">
         {!! do_shortcode('[countdown date="02 October 2022" width="600" color="#F45F2D" bgcolor="#FFF" format="dHMS" hour="07"]') !!}
     </div>
