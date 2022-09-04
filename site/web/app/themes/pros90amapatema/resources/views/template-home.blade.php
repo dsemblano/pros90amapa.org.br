5e314c081c98ad37f973ab5175e898e91466e09c
Daniel Semblano <dsemblano@gmail.com>
2022-09-04 15:01:04 -0300

    section2 removed

--- site/web/app/themes/pros90amapatema/resources/views/template-home.blade.php
+++ site/web/app/themes/pros90amapatema/resources/views/template-home.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 @include('partials/home.section1')
-@include('partials/home.section2')
+{{-- @include('partials/home.section2') --}}
   {{-- {!! do_shortcode('[wdi_feed id="1"]') !!} --}}
   {!! do_shortcode('[instagram-feed feed=1 class="container px-8 lg:px-16"]') !!}
   {{-- @include('partials.allposts') --}}
