fa0655a3bfbe4fecfd014da6637c1052aae7cc7f
Daniel Semblano <dsemblano@gmail.com>
2022-09-05 12:51:44 -0300

    carousel last posts and clock css changes css

--- site/web/app/themes/pros90amapatema/resources/views/template-dias.blade.php
+++ site/web/app/themes/pros90amapatema/resources/views/template-dias.blade.php
@@ -1,16 +1,19 @@
 {{--
   Template Name: Dias Template
 --}}
-<div id="countdown_container" class="container h-auto flex flex-row items-center px-4">
-    @include('partials/snippets.dias')
-    {{-- <a href="{{ home_url('/') }}">
-        <img alt="Logo PROS na home" width="300" src="@asset('images/logodias.jpg')" />
-    </a> --}}
-    <div class="text-3xl  w-2/5 font-bold relative mb-6">
-        {!! do_shortcode('[countdown date="02 October 2022" width="600" color="#F45F2D" bgcolor="#FFF" format="dHMS" hour="07"]') !!}
+<section id="countdown">
+    <div id="countdown_container" class="container h-auto flex flex-row items-center px-4">
+        @include('partials/snippets.dias')
+        {{-- <a href="{{ home_url('/') }}">
+            <img alt="Logo PROS na home" width="300" src="@asset('images/logodias.jpg')" />
+        </a> --}}
+        <div class="text-3xl  w-2/5 font-bold relative mb-6">
+            {!! do_shortcode('[countdown date="02 October 2022" width="600" color="#F45F2D" bgcolor="#FFF" format="dHMS" hour="07"]') !!}
+        </div>
     </div>
-</div>
-<div class="container">
-    {!! do_shortcode('[recent_post_slider design="design-4" autoplay="true" speed="800"]') !!}
-    @include('partials/snippets.clock')
-</div>
+    <div id="recent_clock" class="container">
+        {!! do_shortcode('[recent_post_slider design="design-4" autoplay="true" speed="800"]') !!}
+        @include('partials/snippets.clock')
+    </div>
+</section>
+
